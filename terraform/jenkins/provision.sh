254b90a65a7ed03d39f0ba2b4e5d2d5539bd5e4e
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-03-02 20:33:11 +0100

    build: add provision script #26

---
+++ terraform/jenkins/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum update -y
+yum install -y java-11-amazon-corretto
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum update -y
+yum install -y jenkins
+systemctl enable jenkins
+systemctl start jenkins
+yum install nftables -y
+nft add table nat
+nft add chain ip nat prerouting { type nat hook prerouting  priority 0 \; }
+nft add chain ip nat postrouting { type nat hook postrouting priority 100 \; }
+nft add rule nat prerouting tcp dport 80 redirect to 8080
