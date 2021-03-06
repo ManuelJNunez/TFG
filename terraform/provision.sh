8983ff8a0a74dbeab76add25cc2e3d68b8128f27
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-03-06 17:45:03 +0100

    update provision.sh

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -4,9 +4,12 @@ yum install -y java-11-amazon-corretto
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum update -y
-yum install -y jenkins
+yum install -y jenkins git docker
+usermod -aG docker ec2-user
 systemctl enable jenkins
 systemctl start jenkins
+systemctl enable docker
+systemctl start docker
 yum install nftables -y
 nft add table nat
 nft add chain ip nat prerouting { type nat hook prerouting  priority 0 \; }
