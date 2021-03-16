0d7051a0af43d7913a5ae75419e10b0fe2b3aa64
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-03-16 20:26:19 +0100

    fix: pylint issues close #33

--- src/models/vae.py
+++ src/models/vae.py
@@ -1,13 +1,28 @@
+"""
+VAE implementation using PyTorch. Based in this implementation:
+https://github.com/pytorch/examples/tree/master/vae
+"""
+import torch
 import torch.nn as nn
 
 
 class VAE(nn.Module):
+    """VAE class with variable depth and layer sizes"""
+
     def __init__(self, autoencoder_sizes: [int], classifier_sizes: [int]):
+        """
+        Initialize a new VAE with a classifier.
+
+        Parameters:
+            autoencoder_sizes ([int]): the size of the layers of the encoder of the
+                VAE (the decoder is symmetric).
+            classifier_sizes ([int]): the size of the layers of the latent code classifier.
+        """
         super().__init__()
 
         encoder_layers = []
 
-        for i in range(len(layer_sizes) - 2):
+        for i in range(len(autoencoder_sizes) - 2):
             input_size = autoencoder_sizes[i]
             output_size = autoencoder_sizes[i + 1]
             encoder_layers.append(
@@ -17,7 +32,7 @@ class VAE(nn.Module):
 
         self.encoder = nn.ModuleList(encoder_layers)
 
-        self.mu = nn.Linear(
+        self.mean = nn.Linear(
             in_features=autoencoder_sizes[len(autoencoder_sizes) - 2],
             out_features=autoencoder_sizes[len(autoencoder_sizes) - 1],
         )
@@ -28,7 +43,7 @@ class VAE(nn.Module):
 
         decoder_layers = []
 
-        for i in reversed(range(len(layer_sizes))):
+        for i in reversed(range(len(autoencoder_sizes))):
             input_size = autoencoder_sizes[i]
             output_size = classifier_sizes[i - 1]
             decoder_layers.append(
@@ -60,19 +75,34 @@ class VAE(nn.Module):
         self.classifier = nn.ModuleList(classifier_layers)
 
     def encode(self, encoder_input):
+        """
+        This method computes the encoder output.
+
+        Parameters:
+            encoder_input: data to which we want to reduce the dimensionality.
+        """
         next_input = encoder_input
 
         for layer in self.encoder:
             next_input = layer(next_input)
 
-        return self.mu(next_input), self.logvar(next_input)
+        return self.mean(next_input), self.logvar(next_input)
 
-    def reparametrize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
+    def reparametrize(self):
+        """
+        This method computes the output of the reparametrization trick.
+        """
+        std = torch.exp(0.5 * self.logvar)
         epsilon = torch.randn_like(std)
-        return mu + epsilon * std
+        return self.mean + epsilon * std
 
     def decode(self, latent_code):
+        """
+        This method computes the output of the decoder and of the classifier.
+
+        Parameters:
+            latent code: output from the encoder and reparametrization trick.
+        """
         next_input_decoder = latent_code
 
         for layer in self.decoder:
@@ -86,22 +116,42 @@ class VAE(nn.Module):
         return next_input_decoder, next_input_classifier
 
     def forward(self, vae_input):
-        mu, logvar = self.encode(vae_input)
+        """
+        Method that computes the output of the Autoencoder and the classifier.
 
-        latent_code = self.reparametrize(mu, logvar)
+        Parameter:
+            vae_input: input of the neural network.
+        """
+        mean, logvar = self.encode(vae_input)
 
-        return decode(latent_code), mu, logvar
+        latent_code = self.reparametrize()
+
+        return self.decode(latent_code), mean, logvar
 
     def training_step(self, batch, loss_func):
+        """
+        This method computes the training batch loss.
+
+        Parameters:
+            batch: a smaller portion of data.
+            loss_func: function that computes the training loss in each batch of data.
+        """
         data, label = batch
-        decoder_output, classifier_output, mu, logvar = self(data)
-        loss = loss_func(decoder_output, data, classifier_output, label, mu, logvar)
+        decoder_output, classifier_output, mean, logvar = self(data)
+        loss = loss_func(decoder_output, data, classifier_output, label, mean, logvar)
 
         return loss
 
     def validation_step(self, batch, loss_func):
+        """
+        This method computes the validation batch loss.
+
+        Parameters:
+            batch: a smaller portion of data.
+            loss_func: function that computes the validation loss in each batch of data.
+        """
         data, label = batch
-        decoder_output, classifier_output, mu, logvar = self(data)
-        loss = loss_func(decoder_output, data, classifier_output, label, mu, logvar)
+        decoder_output, classifier_output, mean, logvar = self(data)
+        loss = loss_func(decoder_output, data, classifier_output, label, mean, logvar)
 
         return loss
