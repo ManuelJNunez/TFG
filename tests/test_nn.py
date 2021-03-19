c05c073a6205ff0bd2d4271b88e894754b919829
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-03-19 19:30:49 +0100

    refactor: refactoring some tests #17

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,45 +1,44 @@
 import pytest
+import torch
 import torch.nn as nn
-from torch import Tensor
 from torch import sum
 from src.models.nn import NeuralNetworkModel
 
-def test_initializer():
-    sizes = [5, 4, 3]
-    output_units = 2
+SIZES = [5, 4, 3]
+OUTPUT_UNITS = 2
+SAMPLES = 2
 
-    model = NeuralNetworkModel(sizes, output_units)
-    modules = [module for module in model.layer_list]
+@pytest.fixture
+def model():
+    return NeuralNetworkModel(SIZES, OUTPUT_UNITS)
 
-    for i in range(len(sizes)-1):
-        expected_input = sizes[i]
-        expected_output = sizes[i+1]
+def test_initializer(model):
+    modules = model.layer_list
+
+    for i in range(len(SIZES)-1):
+        expected_input = SIZES[i]
+        expected_output = SIZES[i+1]
         assert modules[i*3].in_features == expected_input
         assert modules[i*3].out_features == expected_output
         assert type(modules[i*3+1]) == type(nn.BatchNorm1d(0))
-        assert modules[i*3+1].weight.size()[0] == expected_output
+        assert modules[i*3+1].num_features == expected_output
         assert type(modules[i*3+2]) == type(nn.ReLU())
 
-    assert modules[-2].in_features == sizes[-1]
-    assert modules[-2].out_features == output_units
+    assert modules[-2].in_features == SIZES[-1]
+    assert modules[-2].out_features == OUTPUT_UNITS
     assert type(modules[-1]) == type(nn.Softmax())
 
 def test_forward():
-    sizes = [5, 4, 3]
-    output_units = 2
-    x = Tensor([[1,2,3,4,5],[6,7,8,9,10]])
+    x = torch.rand(SAMPLES, SIZES[0])
 
-    model = NeuralNetworkModel(sizes, output_units)
+    model = NeuralNetworkModel(SIZES, OUTPUT_UNITS)
     
     predictions = model(x)
 
-    for i in range(len(predictions)):
-        assert predictions[i].sum().item() >= 0.99 and predictions[i].sum().item() <= 1.01
+    assert predictions.sum(dim=1).allclose(torch.ones(SAMPLES))
 
 def test_training_step():
-    x = Tensor([[1,2,3,4,5],[6,7,8,9,10]])
-    sizes = [5, 4, 3]
-    output_units = 2
+    x = torch.rand(SAMPLES, SIZES[0])
     y = 0
 
     batch = (x, y)
@@ -47,15 +46,13 @@ def test_training_step():
     def loss_func(pred, y):
         return pred.sum()
 
-    model = NeuralNetworkModel(sizes, output_units)
+    model = NeuralNetworkModel(SIZES, OUTPUT_UNITS)
     loss = model.training_step(batch, loss_func)
 
     assert loss == loss_func(model(x), y)
 
 def test_validation_step():
-    x = Tensor([[1,2,3,4,5],[6,7,8,9,10]])
-    sizes = [5, 4, 3]
-    output_units = 2
+    x = torch.rand(SAMPLES, SIZES[0])
     y = 0
 
     batch = (x, y)
@@ -63,7 +60,7 @@ def test_validation_step():
     def loss_func(pred, y):
         return pred.sum()
 
-    model = NeuralNetworkModel(sizes, output_units)
+    model = NeuralNetworkModel(SIZES, OUTPUT_UNITS)
     loss = model.validation_step(batch, loss_func)
 
     assert loss == loss_func(model(x), y)
