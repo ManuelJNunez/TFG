975e4f3151687962bbda64116e1c6aee11fa5528
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-03-29 20:16:09 +0200

    adapt docstring to numpy style for vae #39

--- src/models/vae.py
+++ src/models/vae.py
@@ -5,19 +5,41 @@ https://github.com/pytorch/examples/tree/master/vae
 import torch
 import torch.nn as nn
 from .neuralnet import NeuralNetworkModel
+from typing import Tuple
 
 
 class VAE(nn.Module):
-    """VAE class with variable depth and layer sizes"""
+    """
+    Varitional Autoencoder (VAE) implementation with variable depth and layer sizes.
+    This Autoencoder has two parts: the encoder (reduces the dimensionality of the data) and the
+    decoded (reconstructs the data with the smallest possible error). The VAE learns the mean
+    and the variance of the data and construct the latent-code using the reparametrization trick.
+    A fully-connected neural network is used for classifying the latent-code.
+
+    Attributes
+    ----------
+    encoder : nn.ModuleList
+        Contains the layers of the encoder section.
+    mean : nn.Linear
+        The output of this layer is the mean learnt from the data.
+    logvar : nn.Linear
+        The output of this layer is the logvariance learnt from the data.
+    decoder : nn.ModuleList
+        Contains the layers of the decoder section.
+    classifier : NeuralNetworkModel
+        Model that classifies the latent-code (output of the encoder).
+    """
 
     def __init__(self, autoencoder_sizes: [int], classifier_sizes: [int]):
         """
         Initialize a new VAE with a classifier.
 
-        Parameters:
-            autoencoder_sizes ([int]): the size of the layers of the encoder of the
-                VAE (the decoder is symmetric).
-            classifier_sizes ([int]): the size of the layers of the latent code classifier.
+        Parameters
+        ----------
+        autoencoder_sizes : [int]
+            The size of the layers of the encoder of the VAE (the decoder is symmetric).
+        classifier_sizes : [int]
+            The size of the layers of the latent-code classifier.
         """
         super().__init__()
 
@@ -54,12 +76,20 @@ class VAE(nn.Module):
             classifier_sizes[:-1], classifier_sizes[-1]
         )
 
-    def encode(self, encoder_input: torch.Tensor) -> torch.Tensor:
+    def encode(self, encoder_input: torch.Tensor) -> Tuple[torch.Tensor]:
         """
-        This method computes the encoder output.
+        This method computes the encoder's output.
 
-        Parameters:
-            encoder_input: data to which we want to reduce the dimensionality.
+        Parameters
+        ----------
+        encoder_input : torch.Tensor
+            Data to which we want to reduce the dimensionality.
+        
+        Returns
+        -------
+        tuple
+            The first position of the tuple contains the tensor with the learnt mean and the
+            second position contains the learnt logvariance.
         """
         next_input = encoder_input
 
@@ -72,6 +102,18 @@ class VAE(nn.Module):
     def reparametrize(self, mean: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
         """
         This method computes the output of the reparametrization trick.
+
+        Parameters
+        ----------
+        mean : torch.Tensor
+            Tensor that contains the mean learnt from the data.
+        logvar : torch.Tensor
+            Tensor that contains the logvariance learnt from the data.
+
+        Returns
+        -------
+        torch.Tensor
+            Retrieves the latent-code using the reparametrization trick.
         """
         std = torch.exp(0.5 * logvar)
         epsilon = torch.randn_like(std)
@@ -81,8 +123,16 @@ class VAE(nn.Module):
         """
         This method computes the output of the decoder.
 
-        Parameters:
-            latent code: output from the encoder and reparametrization trick.
+        Parameters
+        ----------
+        latent code : torch.Tensor
+            Output tensor from the encoder and reparametrization trick.
+
+        Returns
+        -------
+        torch.Tensor
+            Tensor that contains the output of the decoder (should have the same size as the
+            input).
         """
         next_input = latent_code
 
@@ -95,17 +145,34 @@ class VAE(nn.Module):
         """
         This method computes the output of the classifier.
 
-        Parameters:
-            latent code: output from the encoder and reparametrization trick.
+        Parameters
+        ----------
+        latent code : torch.Tensor
+            Output from the encoder and reparametrization trick.
+
+        Returns
+        -------
+        torch.Tensor
+            The output of the classifier whose size is `num samples` x `num clases`.
         """
         return self.classifier(latent_code)
 
-    def forward(self, vae_input: torch.Tensor) -> torch.Tensor:
+    def forward(self, vae_input: torch.Tensor) -> Tuple[torch.Tensor]:
         """
         Method that computes the output of the Autoencoder and the classifier.
 
-        Parameter:
-            vae_input: input of the neural network.
+        Parameters
+        ----------
+        vae_input: input of the neural network.
+
+        Returns
+        -------
+        tuple
+            This tuple contains the next objects:
+                1. Decoder output
+                2. Classifier output
+                3. Mean learnt from data
+                4. Logvariance learnt from data
         """
         mean, logvar = self.encode(vae_input)
 
@@ -117,9 +184,18 @@ class VAE(nn.Module):
         """
         This method computes the training batch loss.
 
-        Parameters:
-            batch: a smaller portion of data.
-            loss_func: function that computes the training loss in each batch of data.
+        Parameters
+        ----------
+        batch : torch.Tensor
+            A tensor with a portion of data.
+        loss_func : callable
+            Function that computes the training loss in each batch of data.
+
+        Returns
+        -------
+        torch.Tensor
+            The result of the loss functions for the batch of data, the output size depends on
+            the used loss function.
         """
         data, label = batch
         decoder_output, classifier_output, mean, logvar = self(data)
@@ -131,9 +207,18 @@ class VAE(nn.Module):
         """
         This method computes the validation batch loss.
 
-        Parameters:
-            batch: a smaller portion of data.
-            loss_func: function that computes the validation loss in each batch of data.
+        Parameters
+        ----------
+        batch : torch.Tensor
+            A tensor with a portion of data.
+        loss_func : callable
+            Function that computes the training loss in each batch of data.
+
+        Returns
+        -------
+        torch.Tensor
+            The result of the loss functions for the batch of data, the output size depends on
+            the used loss function.
         """
         data, label = batch
         decoder_output, classifier_output, mean, logvar = self(data)
