31c4b03a226c652044cd96eaff2351bf64b90afb
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-04-27 20:40:02 +0200

    refactor: the data loader retrieves the PyTorch's DataLoader (#59)

--- src/ml/utils/snapperml_data_loader.py
+++ src/ml/utils/snapperml_data_loader.py
@@ -1,16 +1,33 @@
-from snapper_ml import DataLoader
-from .utils import read_data
+import snapper_ml
+from typing import Tuple
+from .utils import read_data, default_device
+from .device_data_loader import DeviceDataLoader
+from torch.utils.data import TensorDataset, DataLoader
+from torch import Tensor
+from torch import device
 from pathlib import Path
 
 
-class SnapperDataLoader(DataLoader):
+class SnapperDataLoader(snapper_ml.DataLoader):
     @classmethod
-    def load_data(cls):
+    def load_data(cls, dev: device, batch_size: int):
         train_path = Path("data/Train_EnergyGround_alt5200m_qgsii_fluka_N44971.h5")
         test_path = Path("data/Test_EnergyGround_alt5200m_qgsii_fluka_N14989.h5")
 
         train_data, train_labels = read_data(train_path)
-
         test_data, test_labels = read_data(test_path)
 
-        return train_data, train_labels, test_data, test_labels
+        train_data.unsqueeze_(1)
+        test_data.unsqueeze_(1)
+
+        # Create a Tensor Dataset and a Data Loader for the training data
+        train_ds = TensorDataset(train_data, train_labels)
+        train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+        train_dl = DeviceDataLoader(train_dl, dev)
+
+        # Create a Tensor Dataset and a Data Loader for the test data
+        test_ds = TensorDataset(test_data, test_labels)
+        test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=True)
+        test_dl = DeviceDataLoader(test_dl, dev)
+
+        return train_dl, test_dl, train_data.size()
