45a9c40dae78dbc4565e857a1950b64e51279fce
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2021-05-13 17:14:02 +0200

    add config.py (#63)

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import subprocess
+from pathlib import Path
+
+command = ["git", "rev-parse", "--show-toplevel"]
+
+repo_root = subprocess.run(command, capture_output=True).stdout
+repo_root = Path(repo_root[:-1].decode("utf-8"))
+
+artifacts_path = repo_root.joinpath("artifacts")
+
+artifacts_path.mkdir(exist_ok=True)
+
+model_path = artifacts_path.joinpath("model.pth")
